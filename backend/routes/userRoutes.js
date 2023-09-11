bdd7b89c78e78cf8f243389738ea4e3ab2bfbe37
Vib-UX <vibhav@instaraise.io>
2023-09-12 01:28:12 +0800

    metamask login/register

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,4 +11,33 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Register Route
+router.post("/register-login", async (req, res) => {
+  try {
+    const { turf_id, address } = req.body;
+
+    // Validation
+    if (!turf_id || !address) {
+      return res
+        .status(400)
+        .json({ error: "Both turf_id and address are required." });
+    }
+
+    // Check if user already exists
+    let user = await User.findOne({ where: { address } });
+
+    if (!user) {
+      // Register the user if they don't exist
+      user = await User.create({
+        turf_id,
+        address,
+      });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = router;

14ca86fbc7df2c1cc260fb3da242a2738758ee2b
Vib-UX <vibhav@instaraise.io>
2023-09-12 11:12:34 +0800

    cors

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,9 @@ const userRoutes = require("./routes/userRoutes");
 const gameRoutes = require("./routes/gameRoutes");
 const app = express();
 const PORT = 3000;
-
+const cors = require("cors");
 app.use(express.json());
-
+app.use(cors());
 // Test Route
 app.get("/", (req, res) => {
   res.send("Test Route");
